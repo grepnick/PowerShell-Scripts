099b6fd63aa681d59877c01208d2f263c4cb0310
grepnick <124594745+grepnick@users.noreply.github.com>
2023-02-06 08:49:16 -0600

    Create Rename-Software-Distribution.ps1

---
+++ Rename-Software-Distribution.ps1
@@ -0,0 +1,19 @@
+# PowerShell Script to rename the Windows SoftwareDistribution folder which can help solve update issues.
+$oldName = "$env:SystemRoot\SoftwareDistribution"
+$newName = "$env:SystemRoot\SoftwareDistribution_Old"
+
+Stop-Service -Name "wuauserv" -Force
+Stop-Service -Name "bits" -Force
+
+sleep 15
+
+If (Test-Path $oldName) {
+  Rename-Item $oldName $newName -Force
+  Write-Host "SoftwareDistribution folder renamed to $newName"
+}
+Else {
+  Write-Host "SoftwareDistribution folder not found"
+}
+
+Start-Service -Name "wuauserv"
+Start-Service -Name "bits"
