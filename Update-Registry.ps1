a9d1bf997b417a549ea72352d01bf289063851ab
grepnick <124594745+grepnick@users.noreply.github.com>
2023-02-13 12:54:05 -0600

    Update Update-Registry.ps1

--- Update-Registry.ps1
+++ Update-Registry.ps1
@@ -1,14 +1,22 @@
 # PowerShell script template that checks if a registry item exists and changes it, or creates it if it doesn't exist.
+
 $registryPath = "HKEY_LOCAL_MACHINE\SOFTWARE\YourKey"
 $registryName = "YourValueName"
 $newValue = "YourNewValue"
 
+# Valid value types: String, ExpandString, Binary, DWord, MultiString, Qword, Unknown
+$valueType = "DWORD"
+
+function UpdateRegistry {
+    Set-ItemProperty -Path $registryPath -Name $registryName -Value $newValue -Type $valueType
+}
+
 If (Test-Path $registryPath) {
-  Set-ItemProperty -Path $registryPath -Name $registryName -Value $newValue
+  UpdateRegistry
   Write-Host "Registry item '$registryName' has been updated."
 }
 Else {
   New-Item -Path $registryPath -Force | Out-Null
-  Set-ItemProperty -Path $registryPath -Name $registryName -Value $newValue
+  UpdateRegistry
   Write-Host "Registry item '$registryName' has been created."
 }
