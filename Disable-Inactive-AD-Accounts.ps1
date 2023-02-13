2cdbdb478949ec38a0b2077e8a2db52ee460dc55
grepnick <124594745+grepnick@users.noreply.github.com>
2023-02-13 12:08:30 -0600

    Update Disable-Inactive-AD-Accounts.ps1

--- Disable-Inactive-AD-Accounts.ps1
+++ Disable-Inactive-AD-Accounts.ps1
@@ -1,7 +1,11 @@
+# PowerShell script to disable accounts older than 90 days and move the 
+
 $inactiveThreshold = (Get-Date).AddDays(-90)
+$ou = "OU=Disabled Users,DC=yourdomain,DC=com"
 
 Get-ADUser -Filter { LastLogonTimeStamp -lt $inactiveThreshold } -Properties LastLogonTimeStamp | 
 ForEach-Object { 
   Set-ADUser $_.DistinguishedName -Enabled $false 
-  Write-Host "Account for user '$($_.Name)' has been disabled." 
+  Move-ADObject $_.DistinguishedName -TargetPath $ou
+  Write-Host "Account for user '$($_.Name)' has been disabled and moved to $ou." 
 }
