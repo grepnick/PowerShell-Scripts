b90c9fec77e1e1f389a0b796dd42f3c204b701e2
grepnick <124594745+grepnick@users.noreply.github.com>
2023-02-15 11:46:21 -0600

    Create wifi-stat.ps1

---
+++ wifi-stat.ps1
@@ -0,0 +1,13 @@
+# It's kind of like iostat, but for wifi... and for Windows.
+
+Write-Host "`n`nWireless statistics (press CTRL+C to stop):`n`n"
+
+while ($true) {
+    $signalStrength = (netsh wlan show interfaces) -Match '^\s+Signal' -Replace '^\s+Signal\s+:\s+',''
+    $wirelessBand = (netsh wlan show interfaces) -Match '^\s+Band' -Replace '^\s+Band\s+:\s+',''
+    $wirelessChannel = (netsh wlan show interfaces) -Match '^\s+Channel' -Replace '^\s+Channel\s+:\s+',''
+    $receiveRate = (netsh wlan show interfaces) -Match '^\s+Receive' -Replace '^\s+Receive\s+rate\s+\(Mbps\)\s+:\s+',''
+    $transmitRate = (netsh wlan show interfaces) -Match '^\s+Transmit' -Replace '^\s+Transmit\s+rate\s+\(Mbps\)\s+:\s+',''
+    Write-Host "Signal Strength: $signalStrength | Transmit Rate: $transmitRate Mbps | Receive Rate: $receiveRate Mbps | Wireless Band: $wirelessBand | Channel: $wirelessChannel"
+    Start-Sleep -Seconds 1
+}
