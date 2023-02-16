e24c0d50edd331d3b985019ecff69e3cdc8cac06
grepnick <124594745+grepnick@users.noreply.github.com>
2023-02-15 23:03:06 -0600

    Update Wifi-Stat.ps1

--- Wifi-Stat.ps1
+++ Wifi-Stat.ps1
@@ -3,12 +3,24 @@
 Write-Host "`n`nWireless statistics (press CTRL+C to stop):`n`n"
 
 while ($true) {
-    $signalStrength = (netsh wlan show interfaces) -Match '^\s+Signal' -Replace '^\s+Signal\s+:\s+',''
-    $wirelessBand = (netsh wlan show interfaces) -Match '^\s+Band' -Replace '^\s+Band\s+:\s+',''
-    $wirelessChannel = (netsh wlan show interfaces) -Match '^\s+Channel' -Replace '^\s+Channel\s+:\s+',''
-    $receiveRate = (netsh wlan show interfaces) -Match '^\s+Receive' -Replace '^\s+Receive\s+rate\s+\(Mbps\)\s+:\s+',''
-    $transmitRate = (netsh wlan show interfaces) -Match '^\s+Transmit' -Replace '^\s+Transmit\s+rate\s+\(Mbps\)\s+:\s+',''
-    $wirelessBSSID = (netsh wlan show interfaces) -Match '^\s+BSSID' -Replace '^\s+BSSID\s+:\s+',''
-    Write-Host "Signal Strength: $signalStrength | TX Rate: $transmitRate Mbps | RX Rate: $receiveRate Mbps | Band: $wirelessBand | Channel: $wirelessChannel | AP: $wirelessBSSID"
+    $output = netsh wlan show interfaces
+    $name = $output | Select-String "Name" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $description = $output | Select-String "Description" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $guid = $output | Select-String "GUID" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $physical_address = $output | Select-String "Physical address" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $state = $output | Select-String "State" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $ssid = $output | Select-String "SSID" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $bssid = $output | Select-String "BSSID" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $network_type = $output | Select-String "Network type" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $radio_type = $output | Select-String "Radio type" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $authentication = $output | Select-String "Authentication" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $cipher = $output | Select-String "Cipher" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $connection_mode = $output | Select-String "Connection mode" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $wirelessBand = $output | Select-String "Band" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $wirelessChannel = $output | Select-String "Channel" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $receiveRate = $output | Select-String "Receive rate" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $transmitRate = $output | Select-String "Transmit rate" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    $signalStrength = $output | Select-String "Signal" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+    Write-Host "Signal Strength: $signalStrength | TX Rate: $transmitRate Mbps | RX Rate: $receiveRate Mbps | Band: $wirelessBand | Channel: $wirelessChannel | AP: $bssid"
     Start-Sleep -Seconds 1
 }
