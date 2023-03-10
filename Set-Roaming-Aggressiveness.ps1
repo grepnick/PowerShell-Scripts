24e9fe7cdb0e2c9be142befe1c6c5ba2134948e1
grepnick <124594745+grepnick@users.noreply.github.com>
2023-03-10 15:32:05 -0600

    Update Set-Roaming-Aggressiveness.ps1

--- Set-Roaming-Aggressiveness.ps1
+++ Set-Roaming-Aggressiveness.ps1
@@ -2,38 +2,64 @@
 
 # Credit: Nick Marsh
 
+# Sets two of the most common roaming settings depending on the card manufacturer:
+# RoamAggressiveness = Intel
+# RegROAMSensitiveLevel = Realtek
+
 # Set the $RoamingValue which is the RoamAggressiveness RegistryValue
 # Note: The RegistryValue for Roaming is an ordnal number and the DisplayValue is a cardinal number so 0=1, 1=2, etc.
-# A RegistryValue of 3 is a DisplayValue of "4. Medium-High"
-$RoamingValue = 3
+# Valid values:
+# 0 = "1. Lowest"
+# 1 = "2. Medium-low"
+# 2 = "3. Medium"
+# 3 = "4. Medium-High"
+# 4 = "5. Highest"
+$RoamingAggressiveness = 3
 
-# Find the active wireless adapter
+# Set $RoamingSensitive which is the RegROAMSensitiveLevel RegistryValue
+# Valid values are:
+# 127 = Disabled
+# 65 = High
+# 70 = Middle
+# 80 = Low
+$RoamingSensitive = 70
+
 $adapter = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' -and $_.MediaType -like '*802.11*' }
 
-if ($adapter) {
-    # Check if the Roaming Aggressiveness property exists
-    $property = Get-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RoamAggressiveness" -ErrorAction SilentlyContinue
+try {
+    $RoamingAggressivenessValue = Get-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RoamAggressiveness" -ErrorAction Stop
+}
+catch {
+    Write-Host "RoamAggressiveness advanced property not found on adapter $($adapter.Name)."
+    $RoamingAggressivenessValue = $null
+}
 
-    if ($property) {
-        # Get the current Roaming Aggressiveness property value
-        $beforeValue = $property.DisplayValue
-
-        # Check if the Roaming Aggressiveness property value is already set to $RoamingValue
-        if ($property.RegistryValue -eq $RoamingValue) {
-            Write-Host "Roaming Aggressiveness is already set to $RoamingValue."
-            exit
-        }
-        Set-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RoamAggressiveness" -RegistryValue $RoamingValue
-
-        # Get the new Roaming Aggressiveness property value
-        $afterValue = (Get-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RoamAggressiveness").DisplayValue
-
-        # Output the before and after values
-        Write-Host "Roaming Agressiveness Before: $($beforeValue)"
-        Write-Host "Roaming Agressiveness After: $($afterValue)"
+if ($RoamingAggressivenessValue) {
+    if ($RoamingAggressivenessValue.RegistryValue -eq $RoamingAggressiveness) {
+        Write-Host "RoamAggressiveness is already set to $RoamingAggressiveness on adapter $($adapter.Name)."
     } else {
-        Write-Host "Roaming Aggressiveness property not found."
+        $RoamingAggressivenessValueBefore = $RoamingAggressivenessValue.DisplayValue
+        Set-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RoamAggressiveness" -RegistryValue $RoamingAggressiveness
+        $RoamingAggressivenessValueAfter = (Get-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RoamAggressiveness").DisplayValue
+        Write-Host "RoamAggressiveness value before: $($RoamingAggressivenessValueBefore), after: $($RoamingAggressivenessValueAfter)"
     }
-} else {
-    Write-Host "No active wireless adapter found."
+}
+
+try {
+    $RoamingSensitiveValue = Get-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RegROAMSensitiveLevel" -ErrorAction Stop
+}
+catch {
+    Write-Host "RegROAMSensitiveLevel advanced property not found on adapter $($adapter.Name)."
+    $RoamingSensitiveValue = $null
+}
+
+if ($RoamingSensitiveValue) {
+    if ($RoamingSensitiveValue.RegistryValue -eq $RoamingSensitive) {
+        Write-Host "RegROAMSensitiveLevel is already set to $RoamingSensitive on adapter $($adapter.Name)."
+    } else {
+        $RoamingSensitiveValueBefore = $RoamingSensitiveValue.Value
+        Set-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RegROAMSensitiveLevel" -RegistryValue $RoamingSensitive
+        $RoamingSensitiveValueAfter = Get-NetAdapterAdvancedProperty -Name $adapter.Name -RegistryKeyword "RegROAMSensitiveLevel"
+        Write-Host "RegROAMSensitiveLevel value before: $($RoamingSensitiveValueBefore), after: $($RoamingSensitiveValueAfter)"
+    }
 }
