95844c7a58137fe1dd9c9904ff6d7011de75977e
grepnick <124594745+grepnick@users.noreply.github.com>
2023-04-04 08:11:47 -0500

    Update Rename-Software-Distribution.ps1

--- Rename-Software-Distribution.ps1
+++ Rename-Software-Distribution.ps1
@@ -1,14 +1,16 @@
-# PowerShell Script to rename the Windows SoftwareDistribution folder, which can help solve update issues.
+# PowerShell Script to rename the Windows SoftwareDistribution folder, which can help solve many Windows update issues.
 $oldName = "$env:SystemRoot\SoftwareDistribution"
 $newName = "$env:SystemRoot\SoftwareDistribution_Old"
 
 Stop-Service -Name "wuauserv" -Force
 Stop-Service -Name "bits" -Force
 
-Write-Host "Sleeping for 30 to allow the services to stop and release the directory."
-sleep 30
+sleep 20
 
 If (Test-Path $oldName) {
+  if (Test-Path $newName) {
+    Remove-Item $newName -Recurse -Force
+  }
   Rename-Item $oldName $newName -Force
   Write-Host "SoftwareDistribution folder renamed to $newName"
 }
