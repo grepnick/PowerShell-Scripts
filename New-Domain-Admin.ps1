eb0a84e226713e1390ea5761b5c69925757b8347
grepnick <124594745+grepnick@users.noreply.github.com>
2025-12-07 09:16:44 -0600

    Create New-Domain-Admin.ps1

---
+++ New-Domain-Admin.ps1
@@ -0,0 +1,15 @@
+$Username = "Myadmin"
+$Password = ConvertTo-SecureString "password" -AsPlainText -Force
+$FullName = "My Admin"
+$Group = "Domain Admins"
+
+#Import-Module ActiveDirectory
+
+if (Get-ADUser -Filter {SamAccountName -eq $Username}) {
+    Write-Output "User $Username already exists. Exiting."
+} else {
+
+    New-ADUser -Name $FullName -SamAccountName $Username -AccountPassword $Password -Enabled $true -PassThru
+    Add-ADGroupMember -Identity $Group -Members $Username    
+    Write-Output "User $Username has been created and added to the Domain Admins group."
+}
