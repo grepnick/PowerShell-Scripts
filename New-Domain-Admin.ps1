3457b2ecb8401b41a414e88e491e7fe94fc1124c
grepnick <124594745+grepnick@users.noreply.github.com>
2025-12-07 09:20:00 -0600

    Update New-Domain-Admin.ps1

--- New-Domain-Admin.ps1
+++ New-Domain-Admin.ps1
@@ -9,7 +9,7 @@ if (Get-ADUser -Filter {SamAccountName -eq $Username}) {
     Write-Output "User $Username already exists. Exiting."
 } else {
 
-    New-ADUser -Name $FullName -SamAccountName $Username -AccountPassword $Password -Enabled $true -PassThru
+    New-ADUser -Name $FullName -SamAccountName $Username -AccountPassword $Password -Enabled $true -PasswordNeverExpires $true -PassThru
     Add-ADGroupMember -Identity $Group -Members $Username    
     Write-Output "User $Username has been created and added to the Domain Admins group."
 }
