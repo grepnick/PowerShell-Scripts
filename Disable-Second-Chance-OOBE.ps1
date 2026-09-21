698b26b6e51993e3c7c32b797c4e6342a48f41e5
grepnick <124594745+grepnick@users.noreply.github.com>
2026-09-21 09:16:08 -0500

    Update Disable-Second-Chance-OOBE.ps1

--- Disable-Second-Chance-OOBE.ps1
+++ Disable-Second-Chance-OOBE.ps1
@@ -1,7 +1,13 @@
-# Run a user not system
+# Run and user not SYSTEM
 $Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\UserProfileEngagement'
 New-Item -Path $Path -Force | Out-Null
 New-ItemProperty -Path $Path -Name 'ScoobeSystemSettingEnabled' `
     -PropertyType DWord -Value 0 -Force | Out-Null
 
+
+$Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager'
+New-Item -Path $Path -Force | Out-Null
+New-ItemProperty -Path $Path -Name 'SubscribedContent-310093Enabled' `
+    -PropertyType DWord -Value 0 -Force | Out-Null
+    
 Write-Output "SCOOBE disabled for '$env:USERNAME'."
